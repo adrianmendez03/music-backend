841771d2a86d4ec3ea953f113bb7160868af34bb
Adrian Mendez <adrianmndz328@gmail.com>
2021-02-03 12:49:51 -0600

    CORS test

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('./db/connection')
 const cors = require('cors')
-const corsOptions = require("./configs/cors.js");
 const logger = require('morgan')
 const app = express()
-const { PORT = 3000, NODE_ENV = 'development' } = process.env
+const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
-
-NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
